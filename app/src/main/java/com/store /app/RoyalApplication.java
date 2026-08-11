12cbf00490803ac36d1bb73c3b868212ee7a6ea7
smart346965-cloud <smart346965@gmail.com>
2026-08-11 06:20:51 +0300

    Update RoyalApplication.java

--- app/src/main/java/com/store /app/RoyalApplication.java
+++ app/src/main/java/com/store /app/RoyalApplication.java
@@ -87,7 +87,7 @@ public class RoyalApplication extends Application {
             if (WebViewFeature.isFeatureSupported(WebViewFeature.PRECONNECT)) {
                 androidx.webkit.ProfileStore.getInstance()
                         .getOrCreateProfile("Default")
-                        .preconnect(Uri.parse("https://css-tricks.com").getHost());
+                        .preconnect(Uri.parse("https://snipcart.com").getHost());
                 Log.i("RoyalEngine", "🌐 Preconnect enqueued for origin.");
             }
         } catch (Exception e) {
